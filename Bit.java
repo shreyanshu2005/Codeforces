56e249e4f15c49f61eee7a8ef917e204ef9bc01e
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-02-02 20:58:27 +0530

    Add files via upload
    Bit++

---
+++ Bit.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Bit{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int x = 0;
+    for(int i =0;i<n;i++){
+        String s = sc.next();
+        if(s.equals("++X")){
+            x++;
+        }
+        else if(s.equals("X++")){
+            x++;
+        }
+        else if(s.equals("--X")){
+            x--;
+        }
+        else{
+            x--;
+        }
+
+    }
+    System.out.println(x);
+}
+}
