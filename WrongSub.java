0031d61a94767af9837279caf4a8b510b55ad3eb
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-02-07 19:46:03 +0530

    Add files via upload

---
+++ WrongSub.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class WrongSub {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int k = sc.nextInt();
+    for(int i = 0;i<k;i++){
+        if(n%10 != 0){
+            n=n-1;
+        }
+        else{
+            n = n/10;
+        }
+    }
+    System.out.println(n);
+    }
+    
+    
+}
