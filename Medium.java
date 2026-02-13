4c5b743bd1f0b3523c0929868a89eb88648dbe7a
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-02-13 11:18:03 +0530

    Add files via upload

---
+++ Medium.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Medium {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int [] arr = {a,b,c};
+            Arrays.sort(arr);
+            System.out.println(arr[1]);
+        }
+    }
+}
