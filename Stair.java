64e4aedab9f44bc54eb48d7fad2b9afe3399f5ec
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-03-08 23:00:16 +0530

    Add files via upload

---
+++ Stair.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Stair {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            if(a<b && b<c){
+                System.out.println("STAIR");
+            }
+            else if(a<b && b>c){
+                System.out.println("PEAK");
+            }
+            else{
+                System.out.println("NONE");
+            }
+        }
+    }
+}
