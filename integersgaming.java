6b87ca086342e5e1d80cb363342bf3aa7ca2d7f6
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-05-07 20:27:31 +0530

    Add files via upload

---
+++ integersgaming.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class integersgaming {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0;i<t;i++){
+            int n = sc.nextInt();
+            if(n%3==0){
+                System.out.println("Second");
+            }
+            else{
+                System.out.println("First");
+            }
+        }
+    }
+}
