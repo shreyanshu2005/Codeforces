aed4a7bccef7b929058d69f1a41629d90656e7c1
Shreyanshu Kunwar <163676662+shreyanshu2005@users.noreply.github.com>
2026-06-13 22:29:54 +0530

    Add files via upload

---
+++ Hulk.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Hulk {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                sb.append("I hate");
+            } else {
+                sb.append("I love");
+            }
+
+            if (i == n) {
+                sb.append(" it");
+            } else {
+                sb.append(" that ");
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
